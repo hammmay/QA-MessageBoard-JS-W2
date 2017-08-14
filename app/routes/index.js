90cd1ee0264bceddaa7fbf0dfca466e78e08c309
Laura Hamilton <laurahamilton9@gmail.com>
2017-08-13 21:33:28 -0700

    Add models to both index files

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,4 +1,19 @@
 import Ember from 'ember';
 
+var questions = [{
+  id: 1,
+  author: "Author 1 Example",
+  title: "Title 1 Example",
+  detail: "Detail 1 Example"
+}, {
+  id: 2,
+  author: "Author 2 Example",
+  title: "Title 2 Example",
+  detail: "Detail 2 Example"
+}];
+
 export default Ember.Route.extend({
+  model() {
+    return questions;
+  },
 });
